a1309be72191c5b9f7262f8849c6846ee0990dd4
Murilo Maia <murilomaia.bb@gmail.com>
2020-08-11 00:17:39 -0300

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -15,23 +15,23 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use('/', router);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-
-    console.log(err);
-
-    return response.status(500).json({
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: 'error',
-      message: 'Internal Server Error',
+      message: err.message,
     });
-  },
-);
+  }
+
+  console.log(err);
+
+  throw new Error(err.message);
+
+  // return response.status(500).json({
+  //   status: 'error',
+  //   message: 'Internal Server Error',
+  // });
+});
 
 const port = 3333;
 app.listen(port, () => {
